8434015048f5c89a49d5b8f69ae0072700c52faf
mr4427 <mr4427@rit.edu>
2025-09-23 14:23:55 +0400

    Initial

---
+++ mockassingment.py
@@ -0,0 +1,38 @@
+def function():
+    num = int(input("Enter a number"))
+    if num%2==0:
+        print("The number is even")
+    else:
+        print("The number is odd")
+
+
+
+def function2():
+    num = int(input("Enter a number"))
+    if num>0:
+        print("The number is positive")
+    else:
+        print("The number is odd")
+
+    
+
+def function3():
+    num = int(input("Enter a number"))
+    print(num*num)
+
+
+def function4():
+    num = int(input("Enter a number"))
+    print(num*num*num)
+
+def main():
+    function()
+    function2()
+    function3()
+    function4()
+
+main()
+
+
+
+
