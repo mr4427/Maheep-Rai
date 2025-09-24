e91d8662bb81692d27c3d439ffe22df6b34ff69a
mr4427 <mr4427@rit.edu>
2025-09-24 22:58:25 +0400

    Initial

---
+++ maheep.py
@@ -0,0 +1,34 @@
+def hello():
+    name = input("Enter name") 
+    print("Hello",name) 
+hello()
+
+
+def box():
+    print("*****")
+    print("*   *")
+    print("*****")
+box()
+
+
+def birthdate():
+    print("November 29th, 2006")
+birthdate()
+
+def days_of_week():
+    print("Sunday")
+days_of_week()
+
+def happy_birthday():
+    name = input("Enter name")
+    month = input("Birth month")
+    date = input("Birthdate")
+    year = input("Birth year")
+
+    print(name, ", your birthday is on", month, date, ",", year)
+
+happy_birthday()
+
+ 
+
+ 
